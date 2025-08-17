6a1301dfb4274d854b0e1f29a4c41747014bbe2a
Justin <chenyouduan@gmail.com>
2025-08-17 10:54:52 +0800

    refactor: Remove Cambridge dictionary modules and related tests for cleanup

--- src/main/dictionarys/yahoo/yahoo-voice-download.ts
+++
@@ -1,6 +0,0 @@
-// import VoiceDownload from "../modals/voice-download";
-// export class YahooVoiceDownload extends VoiceDownload {
-//   makeDownloadLink() {
-//     return `https://s.yimg.com/bg/dict/dreye/live/m/${this.volcabulary}.mp3`;
-//   }
-// }
